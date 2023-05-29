df7e6d3fcfc1387392e5ec279709096b8b939ab3
Vandeson Santos <vandeson.eng.quimica@gmail.com>
2023-05-29 11:04:49 -0300

    adicionando js para opacidade da btn-setas para ultima imagem

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,85 @@
-const imagemProcedimento = document.querySelectorAll('.imagem-procedimento');
-const setaAvancar = document.getElementById('btn-avancar');
-const setaVoltar = document.getElementById('btn-voltar');
-let imagemAtual = 0;
+const imagemProcedimento = document.querySelectorAll('.imagem-procedimento')
+const setaAvancar = document.getElementById('btn-avancar')
+const setaVoltar = document.getElementById('btn-voltar')
+let imagemAtual = 0
 
-function esconderImagens(){
-    imagemProcedimento.forEach(imagem => {
-        imagem.classList.remove('mostrar')
-    });
-}
-
-function mostrarImagens(){
-    imagemProcedimento[imagemAtual].classList.add('mostrar');
-}
-
-setaAvancar.addEventListener('click', function(){
-    const totalDeImagens = imagemProcedimento.length - 1;
-    if(imagemAtual === totalDeImagens){
-        return;
+setaAvancar.addEventListener("click", () => {
+    if (imagemAtual === imagemProcedimento.length - 1){
+        return
     }
 
-    imagemAtual++;
+    imagemAtual++
+
+    esconderImagemAberta()
+    mostrarImagem()
+    mostrarOuEsconderSetas()
+})
+
+setaVoltar.addEventListener("click", () => {
+    if (imagemAtual === 0){
+        return
+    }
+
+    imagemAtual--
+
+    esconderImagemAberta()
+    mostrarImagem()
+    mostrarOuEsconderSetas()
+})
+
+function mostrarImagem(){
+    imagemProcedimento[imagemAtual].classList.add("mostrar")
+}
+
+function esconderImagemAberta(){
+    const imagemAberta = document.querySelector(".mostrar")
+    imagemAberta.classList.remove("mostrar")
+}
+
+function mostrarOuEsconderSetas(){
+    const naoEhAhPrimeiraImagem = imagemAtual !== 0
+    if (naoEhAhPrimeiraImagem){
+        setaVoltar.classList.remove("opacidade")
+    } else {
+        setaVoltar.classList.add("opacidade")
+    }
+
+    const chegouNaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagemProcedimento.length - 1
+    if (chegouNaUltimaImagem) {
+        setaAvancar.classList.add("opacidade")
+    } else {
+        setaAvancar.classList.remove("opacidade")
+    }
+}
+
+// function esconderImagens(){
+//     imagemProcedimento.forEach(imagem => {
+//         imagem.classList.remove('mostrar')
+//     });
+// }
+
+// function mostrarImagens(){
+//     imagemProcedimento[imagemAtual].classList.add('mostrar');
+// }
+
+// setaAvancar.addEventListener('click', function(){
+//     const totalDeImagens = imagemProcedimento.length - 1;
+//     if(imagemAtual === totalDeImagens){
+//         return;
+//     }
+
+//     imagemAtual++;
     
-    esconderImagens()
-    mostrarImagens()
-});
+//     esconderImagens()
+//     mostrarImagens()
+// });
 
-setaVoltar.addEventListener('click', function(){
-    if(imagemAtual === 0){
-        return;
-    }
+// setaVoltar.addEventListener('click', function(){
+//     if(imagemAtual === 0){
+//         return;
+//     }
 
-    imagemAtual--;
-    esconderImagens()
-    mostrarImagens()
-});
+//     imagemAtual--;
+//     esconderImagens()
+//     mostrarImagens()
+// });
